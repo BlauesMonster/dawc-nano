545d383b5c9c2cde4abf69d33b1d1367c4ed91ba
BlauesMonster <blauesmonsterdaraw@gmail.com>
2025-04-09 21:41:19 +0200

    feat: docker support

--- src/lib/members-service.ts
+++ src/lib/members-service.ts
@@ -1,13 +1,13 @@
 import type { Member } from './types';
 
 export async function fetchMembers(): Promise<Member[]> {
-	const res = await fetch('/api/members');
+	const res = await fetch('/demo/api/members');
 
 	if (!res.ok) throw new Error('Failed to fetch members');
 	return await res.json();
 }
 
 export async function fetchDefaultAvatar(): Promise<string> {
-	const res = await fetch('/api/defaultAvatarImage');
+	const res = await fetch('/demo/api/defaultAvatarImage');
 	return await res.json();
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter(),
+		paths: {
+			base: '/demo'
+		}}
 };
 
 export default config;
